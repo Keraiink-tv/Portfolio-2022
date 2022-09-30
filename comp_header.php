3b22f7efd5cfe152cd750ac96c3fafc6a9946153
Monica Somera Jensen <80958891+Keraiink-tv@users.noreply.github.com>
2022-09-30 23:39:40 +0200

    Setting it up

---
+++ comp_header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="app.css">
+    <script src="app.js" defer></script>
+    <title>
+        <?php echo $_title ?? 'Upssss..' ?>
+    </title>
+</head>
+
+<body>
+    <div class="frontside_navigation">
+        <h1>Welcome</h1>
+        <p>Hi</p>
+        <p>Test</p>
+        <p>Hej</p>
+        
+        
+        
+        
+    </div>
+</body>
+
